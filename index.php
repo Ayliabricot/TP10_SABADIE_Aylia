8b5fa6795866e8e9a30752207471fa951f4689fc
Aylia <aylia.sabadie@student.junia.com>
2025-03-25 19:15:57 +0100

    Tableaux des élèves et professeurs faits.

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
     echo "</ul>";
 
-    $resultat = $dbPDO->prepare("SELECT * FROM professeurs INNER JOIN matiere ON professeurs.id_matiere=matiere.id INNER JOIN classes ON professeurs.id_classe=classes.id");
+    $resultat = $dbPDO->prepare("SELECT professeurs.id AS id,professeurs.prenom AS prenom,professeurs.nom AS nom,matiere.lib AS matiere,classes.libelle AS classe FROM professeurs INNER JOIN matiere ON professeurs.id_matiere=matiere.id INNER JOIN classes ON professeurs.id_classe=classes.id");
     $resultat->execute();
 
     $professeurs=$resultat->fetchAll();
@@ -32,6 +32,82 @@
 
 
 ?>
+
+
+<br><h3>Les professeurs :</h3>
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Prénom
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Nom
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Matière
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Classe
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+          <?php
+          foreach ($professeurs as $professeur){
+            echo'<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">'.$professeur["id"].'</th>
+                <td class="px-6 py-4">'.$professeur["prenom"].'</td>
+                <td class="px-6 py-4">'.$professeur["nom"].'</td>
+                <td class="px-6 py-4">'.$professeur["matiere"].'</td>
+                <td class="px-6 py-4">'.$professeur["classe"].'</td>
+            </tr>';
+          } 
+          ?>
+        </tbody>
+    </table>
+</div>
+
+<br><h3>Les élèves :</h3>
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Prénom
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Nom
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Classe
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+          <?php
+          foreach ($etudiants as $etudiant){
+            $id=$etudiant['id'];
+            echo'<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">'.$etudiant["id"].'</th>
+                <td class="px-6 py-4">'.$etudiant["prenom"].'</td>
+                <td class="px-6 py-4">'.$etudiant["nom"].'</td>
+                <td class="px-6 py-4">'.$etudiant["classe"].'</td>
+            </tr>';
+          } 
+          ?>
+        </tbody>
+    </table>
+</div>
 
 <h3>Nouvelle matière : </h3>
 <form action="./Views/nouvelle_matiere.php" method="post">
